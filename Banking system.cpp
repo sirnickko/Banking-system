2ef727f308c9864925283120593878d8ab2861f1
sirnickko <ogarinicholas@gmail.com>
2024-12-16 22:07:19 +0300

    Add files via upload

---
+++ Banking system.cpp
@@ -0,0 +1,25 @@
+// Banking system.cpp : Defines the entry point for the application.
+//
+
+#include "Banking system.h"
+#include <stdio.h>
+
+
+using namespace std;
+
+
+struct Account 
+{ int accountNumber; 
+  char name[100]; 
+  char password[50]; 
+  float balance;
+}
+
+int main()
+{
+	
+	
+	
+
+	return 0;
+}
